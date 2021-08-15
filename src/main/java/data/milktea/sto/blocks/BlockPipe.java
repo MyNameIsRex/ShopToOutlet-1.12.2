0641d7c96dc6fde3f3145665801b053eb3e055f6
MyNameIsRex <rex52836@gmail.com>
2021-08-15 13:26:20 +0800

    Update #6 The model for the pipe has been created

--- src/main/java/data/milktea/sto/blocks/BlockPipe.java
+++ src/main/java/data/milktea/sto/blocks/BlockPipe.java
@@ -1,14 +1,17 @@
 package data.milktea.sto.blocks;
 
 import data.milktea.sto.ShopToOutlet;
-import data.milktea.sto.inits.ModBlocks;
-import data.milktea.sto.inits.ModItems;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockPipe extends BlockBase
 {
+    public static final AxisAlignedBB PIPE_AABB = new AxisAlignedBB(0D, (double) 3 / 16, (double) 5 / 16, 1D, (double) 10 / 16, (double) 11 / 16);
+
     public BlockPipe(String name)
     {
         super(name, Material.CLAY);
@@ -18,5 +21,23 @@ public class BlockPipe extends BlockBase
     public void registerModels()
     {
         ShopToOutlet.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return PIPE_AABB;
     }
 }
